440f31c0148c52002744819ab404ef5342224eac
hyezu <20n2155@u.seijo.ac.jp>
2021-08-05 11:38:57 +0000

    2

--- mentor.rb
+++ mentor.rb
@@ -1,40 +1,25 @@
-class C
-  attr_accessor :name
-  def initialize(suffix)
-  self.name = suffix
-  end
-  
-  def job(name)
-    ''
-  end
-end
-
-class Mentor < C
- def initialize(name)
-    super ('煌木')
- end
-
+class Mentor
+  attr_accessor:name
+  def initialize(name)
+    self.name = name
+  end  
   def job(name)
     puts " #{self.name}です。私は現役のITプロフェッショナルです。"
-  end
-end
-
-class RailsMentor < C
-  def initialize(name)
-   super ('赤出')
-  end 
+  end  
+end  
   
-  def job(name)
-   puts " #{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
-  end
+class RailsMentor < Mentor
+  def initialize(name)
+    super('赤出')
+  end  
+  
+ def job(name)
+  puts " #{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+ end
 end
 
 Kirameki = Mentor.new('煌木')
-Akaide = RailsMentor.new ('赤出')
+Akaide = RailsMentor.new('赤出')
 
 Kirameki.job('煌木')
 Akaide.job('赤出')
-
-
-
-  
