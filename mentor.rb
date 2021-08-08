aa4bf1b39ac756665459c116922fd42e212cffb5
hyezu <20n2155@u.seijo.ac.jp>
2021-08-08 05:48:02 +0000

    3

--- mentor.rb
+++ mentor.rb
@@ -1,25 +1,21 @@
 class Mentor
   attr_accessor:name
   def initialize(name)
-    self.name = name
+   self.name = name
   end  
-  def job(name)
+  def job
     puts " #{self.name}です。私は現役のITプロフェッショナルです。"
   end  
 end  
   
 class RailsMentor < Mentor
-  def initialize(name)
-    super('赤出')
-  end  
-  
- def job(name)
+ def job
   puts " #{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
  end
 end
 
-Kirameki = Mentor.new('煌木')
-Akaide = RailsMentor.new('赤出')
+kirameki = Mentor.new('煌木')
+akaide = RailsMentor.new('赤出')
 
-Kirameki.job('煌木')
-Akaide.job('赤出')
+kirameki.job
+akaide.job
